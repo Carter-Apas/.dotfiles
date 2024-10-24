c05bd6210e3afd656a26d680b6afa781fac55ec2
Carter-Apas <57092449+Carter-Apas@users.noreply.github.com>
2024-10-24 14:17:15 +1300

    feat(nvim): Add bigfile

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -12,10 +12,6 @@ require "nvim-treesitter.configs".setup {
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
-    disable = function(lang, bufnr) -- Disable in large C++ buffers
-      return vim.api.nvim_buf_line_count(bufnr) > 10000
-    end,
-
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
