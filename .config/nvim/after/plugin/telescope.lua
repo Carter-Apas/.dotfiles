9dbc5a597293727f7ee5944ea6ec9d016d7febfe
Carter-Apas <57092449+Carter-Apas@users.noreply.github.com>
2025-03-28 15:43:41 +1300

    feat(nvim): Allow tabs from telescope using "t"

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,18 @@
+local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
-require("telescope").setup { defaults = { preview = { treesitter = false } } }
+
+require("telescope").setup({
+  defaults = {
+    preview = { treesitter = false },
+    mappings = {
+      n = {
+        ["t"] = actions.select_tab,
+        ["v"] = actions.select_vertical,
+      },
+    },
+  },
+})
+
 vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>sg", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ss", builtin.grep_string, {})
