b7664db3ff701e59f50f3ad0463af348ef4a7985
Carter Cree <apas.carter@gmail.com>
2025-10-29 18:57:48 +1300

    feat(nvim): Add Redir command

--- .config/nvim/lua/carter/set.lua
+++ .config/nvim/lua/carter/set.lua
@@ -35,6 +35,11 @@ end
 
 -- Add Date
 vim.cmd("command! Date put =strftime('%F')")
-vim.api.nvim_command("augroup FormatBeforeSave")
-vim.api.nvim_command("au!")
-vim.api.nvim_command("augroup END")
+
+vim.api.nvim_create_user_command("Redir", function(ctx)
+  local lines = vim.split(vim.api.nvim_exec(ctx.args, true), "\n",
+    { plain = true })
+  vim.cmd("new")
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.opt_local.modified = false
+end, { nargs = "+", complete = "command" })
