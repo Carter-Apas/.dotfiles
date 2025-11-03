adbf7adec714e932521051f3395760095ba6729d
Carter Cree <apas.carter@gmail.com>
2025-11-04 12:01:53 +1300

    feat(nvim): Grug far ast strict

--- .config/nvim/lua/carter/plugins/grug.lua
+++ .config/nvim/lua/carter/plugins/grug.lua
@@ -1,14 +1,20 @@
 return {
-  {
-    "MagicDuck/grug-far.nvim",
-    opts = {
-      headerMaxWidth = 80,
-      enabledEngines = { "astgrep" },
-      engine = "astgrep"
-    },
-    config = function(_, opts)
-      require("grug-far").setup(opts);
-      vim.keymap.set("n", "<leader>sr", ":botright vertical GrugFar<CR>")
-    end
-  }
+    {
+        "MagicDuck/grug-far.nvim",
+        opts = {
+            headerMaxWidth = 80,
+            enabledEngines = {"astgrep"},
+            engine = "astgrep",
+            engines = {
+                astgrep = {
+                    defaults = {flags = "--strictness=ast"},
+                    placeholders = {enabled = true, flags = "yo"}
+                }
+            }
+        },
+        config = function(_, opts)
+            require("grug-far").setup(opts);
+            vim.keymap.set("n", "<leader>sr", ":botright vertical GrugFar<CR>")
+        end
+    }
 }
