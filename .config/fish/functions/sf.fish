2111156bc209b7daeb87603f0c59b64eb15c9b75
Carter Cree <apas.carter@gmail.com>
2026-02-17 14:28:05 +1300

    fix(fish): Support files with sf

--- .config/fish/functions/sf.fish
+++ .config/fish/functions/sf.fish
@@ -1,3 +1,11 @@
 function sf
-  cd $(find ~ -maxdepth 4 -type d  \( -name node_modules -o -name Library -o -path ./.Trash \) -prune -o -print | fzf)
+    set -l target (find ~ -maxdepth 4 \( -name node_modules -o -name Library -o -path '*/.Trash' \) -prune -o -print | fzf)
+
+    if test -n "$target"
+        if test -f "$target"
+            cd (dirname "$target")
+        else
+            cd "$target"
+        end
+    end
 end
